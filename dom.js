cdf8d180b900f5185d746e8aa4e344fd6039d91b
4athul2p0m <97athulpm@gmail.com>
2022-11-07 13:31:46 +0530

    modified items of class list-items

--- dom.js
+++ dom.js
@@ -8,3 +8,14 @@ console.log(additems);
 additems.style.fontWeight = 'bold';
 //set font color of additems to green
 additems.style.color = 'green'
+
+//items store list of items of class 'list-group-item
+var items = document.getElementsByClassName('list-group-item');
+
+//make 3rd element of list have green background color
+items[2].style.backgroundColor = 'green';
+
+//make all elements of list have bold font
+for(var i=0;i<items.length;i++) {
+    items[i].style.fontWeight = 'bold';
+}
