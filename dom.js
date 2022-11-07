041db2fc6c82681cd180993a0ec7685af935c6f7
4athul2p0m <97athulpm@gmail.com>
2022-11-07 13:04:41 +0530

    modified header styles

--- dom.js
+++ dom.js
@@ -1,1 +1,10 @@
-console.dir(document);
+var header = document.getElementById('main-header');
+//add border to header
+header.style.border = 'solid 3px #000';
+
+var additems = document.getElementById('list-header');
+console.log(additems);
+//set additems font to bold
+additems.style.fontWeight = 'bold';
+//set font color of additems to green
+additems.style.color = 'green'
