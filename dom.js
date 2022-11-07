51c3eb6ceb0f08c53a40e20235653e7d5ae8694d
4athul2p0m <97athulpm@gmail.com>
2022-11-07 16:20:53 +0530

    manipulated using tags and classes

--- dom.js
+++ dom.js
@@ -19,3 +19,15 @@ items[2].style.backgroundColor = 'green';
 for(var i=0;i<items.length;i++) {
     items[i].style.fontWeight = 'bold';
 }
+
+//using getElementByClassName
+
+var item = document.getElementsByClassName('new-list-item');
+item[0].style.fontWeight = 'bold'
+
+//using getelementbytagname
+
+var li = document.getElementsByTagName('li');
+for(var i=0;i<li.length;i++) {
+    li[i].style.backgroundColor = 'grey'
+}
