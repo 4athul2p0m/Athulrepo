8544c14f608f5deb3ea1a32cd886659042751f0f
4athul2p0m <97athulpm@gmail.com>
2022-11-09 11:07:32 +0530

    Registered users is now shown on screen

--- form/main.js
+++ form/main.js
@@ -2,6 +2,8 @@ var submit = document.getElementById('btn');
 var userName = document.getElementById('name');
 var email = document.getElementById('email');
 
+var list = document.getElementById('items');
+
 //submit button click event
 submit.addEventListener('click',storeDetails);
 
@@ -17,13 +19,24 @@ function storeDetails(e) {
             name: userName.value,
             email: email.value
         };
-        console.log(userObj);
 
         //serializing userObj for local storage
         var userObj_serialized = JSON.stringify(userObj);
         localStorage.setItem(userObj.name,userObj_serialized);
         
-        // localStorage.setItem(userName.value,email.value);
-        // console.log(localStorage.getItem(userName.value));
+        //create <li> element
+        var li = document.createElement('li');
+        li.className = 'item';
+        //deserialize userObj
+        var userObj_deserialized = JSON.parse(localStorage.getItem(userObj.name));
+        
+        //append textnode with name and email to <li> element
+        li.appendChild(document.createTextNode(userObj_deserialized.name));
+        //insert space between name and email
+        li.appendChild(document.createTextNode('\u00A0 \u00A0'));
+        li.appendChild(document.createTextNode(userObj_deserialized.email));
+
+        //append to <ul> element to show on page
+        list.appendChild(li);
     }
 }
